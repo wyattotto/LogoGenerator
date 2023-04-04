a5af9ad3fe7c06bed65963d2923db20fd787a6a6
wyatt otto <wyattotto87@gmail.com>
2023-04-04 19:46:20 -0400

    made some changes to the code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const inquirer = require('inquirer')
 const fs = require(`fs`)
-const {circle, square, triangle} = require (`.lib/shapes.js`)
+const shape  = require ('./lib/shapes.js')
 const { kMaxLength } = require("buffer")
 const MaxLengthInputPrompt = require('inquirer-maxLength-input-prompt')
-
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 
 
 
@@ -12,17 +12,17 @@ const questions = [
         type: "list",
         name: "shape",
         message: "Select the shape for your logo",
-        Choices: ['square','triangle','circle']
+        choices: ['square','triangle','circle']
     },
     {
         type: "input",
         name: "color",
-        message: "Input the color you want for your logo"
+        message: "Input the color you want for your logo."
     },
     {
         type: "MaxLengthInputPrompt",
         name: 'LogoName',
-        message: "Input 3 characters for your logo",
+        message: "Input 3 characters for your logo.",
         maxLength: 3,
     },
     {
@@ -32,9 +32,9 @@ const questions = [
     },
 ]
 
-const prompt = inquire.createPromptModule();
+const prompt = inquirer.createPromptModule();
 
-function writeToFile(fileName, data){
+function writeToFile (fileName, data) {
     fs.writeFile(fileName, data, function (err){
         if (err) {
             return console.log(err);
@@ -46,9 +46,9 @@ function writeToFile(fileName, data){
 function init() {
     inquirer.prompt(questions).then((res) =>{
         console.log("Your Logo is being made as we speak!")
-        fs.writeToFile(`example/newLogo.svg`, shape.newLog(res),
+        fs.writeFile(`examples/NewLogo.svg`, shape.generateLogo(res),
         err =>{
-         err ? console.err() : console.log('SVG MADE')
+         err ? console.log(err) : console.log('SVG MADE')
         });
 
     })
